04faca71e2b8d390d793b685a73e9f14ec0dcef8
jaimemin <jaimemin@gmail.com>
2021-03-11 02:10:28 +0900

    2021.03.11 (Bronze4)

--- BOJ 5543.cpp
+++ BOJ 5543.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int main(void)
 {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
+	int topBurger, middleBurger, bottomBurger, cola, cider;
+	cin >> topBurger >> middleBurger >> bottomBurger >> cola >> cider;
 
+	int result = min(topBurger, min(middleBurger, bottomBurger)) + min(cola, cider) - 50;
+
+	cout << result << "\n";
+
+	return 0;
 }
