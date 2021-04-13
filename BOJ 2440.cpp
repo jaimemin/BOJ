0a6f565eabb66f7477b43b56a8a171a87ce32962
jaimemin <jaimemin@gmail.com>
2021-04-14 02:16:57 +0900

    2021.04.14 (Bronze3)
    별 찍기 1 ~ 5

---
+++ BOJ 2440.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int N;
+	cin >> N;
+
+	for (int i = N; i >= 1; i--)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			cout << "*";
+		}
+
+		cout << "\n";
+	}
+
+	return 0;
+}
