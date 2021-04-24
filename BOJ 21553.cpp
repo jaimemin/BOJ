0e28ed00a72e8577f2c02aa4dc9c11a234000d0d
jaimemin <jaimemin@gmail.com>
2021-04-24 22:42:16 +0900

    2021.04.24 (Bronze4)

---
+++ BOJ 21553.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	string A, P;
+	cin >> A >> P;
+
+	cout << P << "\n";
+
+	return 0;
+}
