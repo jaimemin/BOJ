478f99b55ad87ecf192472655a5f97e538e02555
jaimemin <jaimemin@gmail.com>
2021-07-06 00:20:32 +0900

    2021.07.06 (Bronze3)

---
+++ BOJ 9316.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int N;
+	cin >> N;
+
+	for (int i = 1; i <= N; i++)
+	{
+		printf("Hello World, Judge %d!\n", i);
+	}
+
+	return 0;
+}
