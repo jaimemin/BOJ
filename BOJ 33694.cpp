f86b0e119ec9d06a33ff932fd7ab8aa06c41b3dd
jaimemin <jaimemin@gmail.com>
2025-04-19 23:51:12 +0900

    2025.04.19

--- BOJ 33694.cpp
+++ BOJ 33694.cpp
@@ -3,97 +3,180 @@
 #include <algorithm>
 using namespace std;
 
-typedef long long ll;
- 
-struct Event 
-{
-    ll day;
-    ll delta;
-};
+const long long MOD = 1e9 + 7;
 
-bool cmp(Event a, Event b)
+vector<int> parent;
+
+vector<int> ranks;
+
+vector<int> xor2Parent;
+
+vector<int> fixedVal;
+
+bool conflict = false;
+
+int findSet(int x)
 {
-    if (a.day == b.day)
+    if (parent[x] == x)
     {
-        return a.delta < b.delta;
+        return x;
+    }
+
+    int p = parent[x];
+    parent[x] = findSet(p);
+    xor2Parent[x] ^= xor2Parent[p];
+
+    return parent[x];
+}
+
+void unionSet(int x, int y, int w)
+{
+    int rx = findSet(x), ry = findSet(y);
+
+    if (rx == ry)
+    {
+        if ((xor2Parent[x] ^ xor2Parent[y]) != w)
+        {
+            conflict = true;
+        }
+
+        return;
+    }
+
+    int d = xor2Parent[x] ^ xor2Parent[y] ^ w;
+
+    if (ranks[rx] < ranks[ry])
+    {
+        swap(rx, ry);
+    }
+
+    parent[ry] = rx;
+    xor2Parent[ry] = d;
+
+    if (fixedVal[rx] == -1 && fixedVal[ry] != -1) 
+    {
+        fixedVal[rx] = fixedVal[ry] ^ d;
+    }
+    else if (fixedVal[rx] != -1 && fixedVal[ry] != -1) 
+    {
+        if (fixedVal[rx] != (fixedVal[ry] ^ d))
+        {
+            conflict = true;
+        }
     }
     
-    return a.day < b.day;
+    if (ranks[rx] == ranks[ry])
+    {
+        ranks[rx]++;
+    }
 }
- 
-int main(){
+
+void fixNode(int x, int val) 
+{
+    int rx = findSet(x);
+    int desired = val ^ xor2Parent[x];
+
+    if (fixedVal[rx] == -1)
+    {
+        fixedVal[rx] = desired;
+    }
+    else if (fixedVal[rx] != desired)
+    {
+        conflict = true;
+    }
+}
+
+long long modexp(long long base, long long exp)
+{
+    long long result = 1;
+    base %= MOD;
+
+    while (exp > 0) 
+    {
+        if (exp & 1)
+        {
+            result = (result * base) % MOD;
+        }
+
+        base = (base * base) % MOD;
+        exp >>= 1;
+    }
+
+    return result;
+}
+
+int main() 
+{
     ios::sync_with_stdio(false);
     cin.tie(0);
-    int n;
-    ll X;
-    cin >> n >> X;
-    
-    vector<Event> events;
-    events.reserve(2LL * n);
- 
-    for (int i = 0; i < n; i++)
+    int N, M;
+    cin >> N >> M;
+    parent.resize(N + 1);
+    ranks.assign(N + 1, 0);
+    xor2Parent.assign(N + 1, 0);
+    fixedVal.assign(N + 1, -1);
+
+    for (int i = 1; i <= N; i++)
     {
-        ll A, B, C, T;
-        cin >> A >> B >> C >> T;
- 
-        ll daysToReachC = (C + A - 1) / A;
-        ll tR = T + daysToReachC;
-        ll maxSize = A * (tR - T);
-        
-        if(maxSize < X) 
-        {
-            continue;
-        }
-        
-        ll daysToReachX = (X + A - 1) / A;
-        ll L = T + daysToReachX;
-        ll extra = (maxSize - X) / B;
-        ll R = tR + extra;
- 
-        if(L > R) 
-        {
-            continue;
-        }
-        
-        events.push_back({L, 1});
-        events.push_back({R + 1, -1});
+        parent[i] = i;
     }
- 
-    if(events.empty())
+
+    for (int i = 0; i < M; i++)
+    {
+        int a, b;
+        char type;
+        cin >> a >> b >> type;
+
+        if (conflict)
+        {
+            continue;
+        }
+
+        if (type == 'D') 
+        {
+            unionSet(a, b, 1);
+        }
+        else if (type == 'U')
+        {
+            unionSet(a, b, 0);
+            fixNode(a, 0);
+            fixNode(b, 0);
+        }
+        else if (type == 'P') 
+        {
+            unionSet(a, b, 0);
+            fixNode(a, 1);
+            fixNode(b, 1);
+        }
+    }
+
+    if (conflict)
     {
         cout << 0 << "\n";
-        
+
         return 0;
     }
- 
-    sort(events.begin(), events.end(), cmp);
- 
-    ll currCount = 0;
-    ll answer = 0;
-    ll prevDay = events[0].day;
-    int idx = 0;
-    int sizeEvents = events.size();
- 
-    while(idx < sizeEvents)
+
+    vector<bool> seen(N + 1, false);
+    int freeCount = 0;
+
+    for (int i = 1; i <= N; i++)
     {
-        ll currentDay = events[idx].day;
+        int r = findSet(i);
 
-        if(currCount >= 3)
+        if (!seen[r])
         {
-            answer += (currentDay - prevDay);
-        }
+            seen[r] = true;
 
-        while(idx < sizeEvents && events[idx].day == currentDay)
-        {
-            currCount += events[idx].delta;
-            
-            idx++;
+            if (fixedVal[r] == -1)
+            {
+                freeCount++;
+            }
         }
-        
-        prevDay = currentDay;
     }
- 
-    cout << answer << "\n";
     
+    long long answer = modexp(2, freeCount);
+    cout << answer % MOD << "\n";
+
     return 0;
 }
